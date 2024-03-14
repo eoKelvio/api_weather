345ad7167103caf6a83f638e40128ba1dc971e8f
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-03-14 19:30:22 -0300

    feat: mongoDB connection
    Switch sql server to mongoDB

--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,29 @@
-from datetime import datetime
+import datetime
 from random import randrange
-from django.shortcuts import render
 from django.views import View
-from .models import WeatherEntity
+from django.shortcuts import redirect, render
+from .repositories import WeatherRepository
+from weather.models import WeatherEntity
 
 class WeatherView(View):
+   
     def get(self, request):
-        weathers = []
-        for i in range(10):
-            weathers.append(
-                WeatherEntity(
-                    temperature=randrange(start=17, stop=40),
-                    date=datetime.now()
-                )
-            )
-        return render(request, "home.html")
-        
+        repository = WeatherRepository(collectionName='weathers')
+        weathers = repository.getAll()
+        return render(request, "home.html", {"weathers":weathers})
+    
+
+class WeatherGenerate(View):
+    def get(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        # wheater = WeatherEntity(
+        #     temperature=randrange(start=17, stop=40),
+        #     date=datetime.now()
+        # )
+        wheater = {
+            "temperature" : 28,
+            "date": "hoje"
+            }
+        repository.insert(wheater)
+
+        return redirect('Weather View')

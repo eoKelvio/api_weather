00397b02c34dfcf5b83adfcdb28e644b5c6d8373
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-03-19 19:24:42 -0300

    mongo aplication

--- weather/views.py
+++ weather/views.py
@@ -1,29 +1,31 @@
-import datetime
+#views.py
+from datetime import datetime
 from random import randrange
 from django.views import View
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from .models import WeatherEntity
 from .repositories import WeatherRepository
-from weather.models import WeatherEntity
 
 class WeatherView(View):
-   
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
-        weathers = repository.getAll()
+        weathers = WeatherSerializer(repository.getAll(), many=True)
         return render(request, "home.html", {"weathers":weathers})
     
 
 class WeatherGenerate(View):
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
-        # wheater = WeatherEntity(
-        #     temperature=randrange(start=17, stop=40),
-        #     date=datetime.now()
-        # )
-        wheater = {
-            "temperature" : 28,
-            "date": "hoje"
-            }
-        repository.insert(wheater)
+        wheater = WeatherEntity(
+            temperature=randrange(start=17, stop=40),
+            date=datetime.now()
+        )
+        serializer = WeatherSerializer(weather)
+        repository.insert(serializer.data)
 
         return redirect('Weather View')
+        
+class WeatherReset(View):
+
+    def get(self, request):
+		    pass
